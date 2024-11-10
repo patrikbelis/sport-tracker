46ece26c32d38369d70fe2ffc9fff9b872da9855
Patrik Belis <patffo@gmail.com>
2024-11-10 12:00:27 +0100

    project init

---
+++ Sport Tracker/Sport Tracker/Sport_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_TrackerApp.swift
+//  Sport Tracker
+//
+//  Created by Patrik on 10/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sport_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
